2889b6872b1ff140b64f532de6bb513ce7cf47a8
Army-R <80074550+Army-R@users.noreply.github.com>
2025-12-18 14:40:46 -0600

    Create function for filling the form

--- bot/booking/booking.py
+++ bot/booking/booking.py
@@ -1,4 +1,5 @@
 # Import the necessary modules
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -31,7 +32,7 @@ class Booking(webdriver.Edge):
         except:
             pass
 
-    def change_currency(self, currency=0):
+    def change_currency(self, currency=0, code='USD'):
         # Open the currency picker
         currency_button = WebDriverWait(self, 10).until(
             EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[data-testid="header-currency-picker-trigger"]'))
@@ -43,4 +44,47 @@ class Booking(webdriver.Edge):
             EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'button[data-testid="selection-item"] span[class*="Picker_selection-text"]'))
         )
         # Click by index
-        currency_items[currency].click() # Default is USD
+        currency_items[currency].click() # Default is American Dollar
+
+        # Validate the currency change
+        selected_currency = WebDriverWait(self, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'button[data-testid="header-currency-picker-trigger"] span[class*="ca2ca5203b"]'))
+        )
+        if selected_currency.text != code:
+            raise Exception(f'Currency did not change to {code}') # Default is USD
+        
+    def destination_form(self, city='Guadalajara'):
+        # Wait for the form and enter destination
+        destination = WebDriverWait(self, 10).until(
+            EC.presence_of_element_located((By.NAME, 'ss'))
+        )
+        destination.clear()
+        destination.send_keys(city) # Default is Guadalajara Jalisco, México
+        
+        # Validate the input
+        if destination.get_attribute('value') != city:
+            raise Exception(f'Destination input failed, expected {city}')
+        
+        # Wait for the dropdown to update and select the first option of the city
+        destination_results = WebDriverWait(self, 10).until(
+            lambda driver: driver.find_element(By.CSS_SELECTOR, 'ul[role="group"] li[id="autocomplete-result-0"]')
+
+            if driver.find_element(By.CSS_SELECTOR, 'ul[role="group"] li[id="autocomplete-result-0"]').text.startswith(city)
+            
+            else False
+        )
+        time.sleep(2) # Wait before clicking destination
+
+        destination_results.click()
+
+        time.sleep(2) # Wait before clikking on the date picker
+
+        # Click on the date picker and Wait for the calendar to load
+        date = WebDriverWait(self, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[data-testid="searchbox-dates-container"]'))
+        )
+        date.click()
+
+        calendar = WebDriverWait(self, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'div[data-testid="searchbox-datepicker"]'))
+        )

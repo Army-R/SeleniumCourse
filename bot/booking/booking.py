f8e1214d848cd37d97d3489283ab913d2f687069
Army-R <80074550+Army-R@users.noreply.github.com>
2025-12-17 19:41:07 -0600

    Add changing currency function

--- bot/booking/booking.py
+++ bot/booking/booking.py
@@ -1,5 +1,8 @@
 # Import the necessary modules
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import booking.constants as const
 
 # Create a new class for booking
@@ -10,7 +13,7 @@ class Booking(webdriver.Edge):
         super(Booking, self).__init__()
         self.maximize_window()
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, *_):
         # Quit the WebDriver session
         if self.teardown:
             self.quit()
@@ -18,3 +21,26 @@ class Booking(webdriver.Edge):
     def land_first_page(self):
         # Navigate to the booking website's homepage
         self.get(const.URL)
+    
+    def close_login_popup(self):
+        try:
+            login_popup = WebDriverWait(self, 10).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[aria-label="Ignorar información sobre el inicio de sesión."]'))
+            )
+            login_popup.click()
+        except:
+            pass
+
+    def change_currency(self, currency=0):
+        # Open the currency picker
+        currency_button = WebDriverWait(self, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[data-testid="header-currency-picker-trigger"]'))
+        )
+        currency_button.click()
+
+        # Wait for the currency list
+        currency_items = WebDriverWait(self, 10).until(
+            EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'button[data-testid="selection-item"] span[class*="Picker_selection-text"]'))
+        )
+        # Click by index
+        currency_items[currency].click() # Default is USD

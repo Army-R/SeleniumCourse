7374853fcccc6fa6359b5a966ee3e341c2da06c6
Army-R <80074550+Army-R@users.noreply.github.com>
2025-12-18 14:41:00 -0600

    Updated with new functions

--- bot/run.py
+++ bot/run.py
@@ -4,4 +4,5 @@ from booking.booking import Booking
 with Booking(teardown=True) as bot:
     bot.land_first_page()
     bot.close_login_popup()
-    bot.change_currency() 
+    #bot.change_currency() 
+    bot.destination_form()

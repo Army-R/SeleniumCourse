458a9d4d246173e8c09ab2a4d94628c5f8e929ba
Army-R <80074550+Army-R@users.noreply.github.com>
2025-12-18 19:24:12 -0600

    Add new functions to the file

--- bot/run.py
+++ bot/run.py
@@ -4,5 +4,8 @@ from booking.booking import Booking
 with Booking(teardown=True) as bot:
     bot.land_first_page()
     bot.close_login_popup()
-    #bot.change_currency() 
-    bot.destination_form()
+    bot.change_currency() 
+    bot.destination_field()
+    bot.pick_dates('2025-12-21', '2026-01-12')
+    bot.occupancy() 
+    bot.search() 
